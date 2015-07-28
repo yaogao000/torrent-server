edbe8e9ebf944d94923bae2ab24db50685322623
yaogao000 <yaogao000@163.com>
2015-07-28 18:24:54 +0800

    修改 requestFunnel结构

--- web/api-customer/src/main/java/com/drink/web/filter/funnel/RequestFunnel.java
+++ web/api-customer/src/main/java/com/drink/web/filter/funnel/RequestFunnel.java
@@ -7,8 +7,12 @@ package com.drink.web.filter.funnel;
  */
 public class RequestFunnel {
 	private String api;
-
-	private RequestFunnelAttr requestFunnelAttr;
+	private String key;
+	private short limit;
+	private short time;
+	private String[] parameters;
+	private short code;
+	private String msg;
 
 	public String getApi() {
 		return api;
@@ -18,11 +22,51 @@ public class RequestFunnel {
 		this.api = api;
 	}
 
-	public RequestFunnelAttr getRequestFunnelAttr() {
-		return requestFunnelAttr;
+	public String getKey() {
+		return key;
 	}
 
-	public void setRequestFunnelAttr(RequestFunnelAttr requestFunnelAttr) {
-		this.requestFunnelAttr = requestFunnelAttr;
+	public void setKey(String key) {
+		this.key = key;
+	}
+
+	public short getLimit() {
+		return limit;
+	}
+
+	public void setLimit(short limit) {
+		this.limit = limit;
+	}
+
+	public short getTime() {
+		return time;
+	}
+
+	public void setTime(short time) {
+		this.time = time;
+	}
+
+	public String[] getParameters() {
+		return parameters;
+	}
+
+	public void setParameters(String[] parameters) {
+		this.parameters = parameters;
+	}
+
+	public short getCode() {
+		return code;
+	}
+
+	public void setCode(short code) {
+		this.code = code;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
 	}
 }
