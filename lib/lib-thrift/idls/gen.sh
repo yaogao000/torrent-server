0e58b420b663d277428938b1b6b289a6306b7f1b
yaogao000 <yaogao000@163.com>
2015-07-28 14:20:52 +0800

    web 项目 portal 引入

--- lib/lib-thrift/idls/gen.sh
+++ lib/lib-thrift/idls/gen.sh
@@ -8,11 +8,11 @@ current=$(cd `dirname $0`; pwd);
 
 for i in `ls $current`
     	do
-		#echo $i;
-		currenti="$current/$i";
-		#echo $currenti;
-           	if [ -e "$currenti/srv.thrift" ];then
-			thrift -out $current/../src/main/java/ -r --gen java $currenti/srv.thrift;
-                    	echo "$i";
-            	fi
+			#echo $i;
+			currenti="$current/$i";
+			#echo $currenti;
+	       	if [ -e "$currenti/srv.thrift" ];then
+				thrift -out $current/../src/main/java/ -r --gen java $currenti/srv.thrift;
+	        	echo "$i";
+	        fi
     	done
