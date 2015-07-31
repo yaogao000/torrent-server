93c82394f58c2865e9b245ddb073165efddf2f28
yaogao000 <yaogao000@163.com>
2015-07-31 11:12:18 +0800

    修复 log4j 无日志输出的bug

--- common/common-web/src/main/java/com/drink/common/web/filter/AbstractRequestFunnelFilter.java
+++ common/common-web/src/main/java/com/drink/common/web/filter/AbstractRequestFunnelFilter.java
@@ -3,7 +3,6 @@ package com.drink.common.web.filter;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
